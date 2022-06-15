3d548bcdf33bfcf2246087a82849a33109e61e7a
Restyled.io <commits@restyled.io>
2022-06-15 01:01:01 +0000

    Restyled by clang-format

--- android/app/src/main/java/info/varden/hauk/global/RebootReceiver.java
+++ android/app/src/main/java/info/varden/hauk/global/RebootReceiver.java
@@ -5,11 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.widget.Toast;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.SocketAddress;
-
 import info.varden.hauk.Constants;
 import info.varden.hauk.R;
 import info.varden.hauk.global.ui.AuthorizationActivity;
@@ -28,21 +23,26 @@ import info.varden.hauk.system.preferences.PreferenceManager;
 import info.varden.hauk.utils.DeprecationMigrator;
 import info.varden.hauk.utils.Log;
 import info.varden.hauk.utils.TimeUtils;
-// Reboot broadcast receiver for Hauk. If enabled resumes a share session if one exists on device restart
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.SocketAddress;
+// Reboot broadcast receiver for Hauk. If enabled resumes a share session if one
+// exists on device restart
 public final class RebootReceiver extends BroadcastReceiver {
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        // Subsequent calls may result in data being read from preferences. We should ensure that
-        // all deprecated preferences have been migrated before we continue.
-        new DeprecationMigrator(context).migrate();
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    // Subsequent calls may result in data being read from preferences. We
+    // should ensure that all deprecated preferences have been migrated before
+    // we continue.
+    new DeprecationMigrator(context).migrate();
 
-        PreferenceManager prefs = new PreferenceManager(context);
-        if(prefs.get(Constants.PREF_RESTART_ON_BOOT))
-            resumeShare(context,intent);
-    }
-    private void resumeShare(Context context, Intent intent) {
-        Log.d("Trying to resume shares...");
-        new BroadcastSessionManager(context).resumeShares();
-    }
+    PreferenceManager prefs = new PreferenceManager(context);
+    if (prefs.get(Constants.PREF_RESTART_ON_BOOT))
+      resumeShare(context, intent);
+  }
+  private void resumeShare(Context context, Intent intent) {
+    Log.d("Trying to resume shares...");
+    new BroadcastSessionManager(context).resumeShares();
+  }
 }
